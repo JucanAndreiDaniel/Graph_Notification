8320581426708f5e625e28e57ea5c3af1a15fbaf
Gabriel Mareș <59911676+GabrielMares00@users.noreply.github.com>
2021-04-06 22:43:38 +0300

    Search tab mockup

--- app/src/main/java/com/example/scfnotificationmockup/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/scfnotificationmockup/ui/dashboard/DashboardViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "Search for your desired thing"
     }
     val text: LiveData<String> = _text
 }
