1a807fb80b917fa3a08e867c525321cf78dcf2de
Gabriel Mareș <gabriel.mares00@e-uvt.ro>
2021-04-06 18:24:51 +0300

    rebase app

--- README.md
+++
@@ -1,2 +0,0 @@
-# Preview
-<img src="https://i.imgur.com/OIP1K57.png" width="50%" height="auto" style="align-items:center;">
--- app/src/main/java/com/testing/nokia19/ui/dashboard/DashboardViewModel.java
+++
@@ -1,8 +0,0 @@
-package com.testing.nokia19.ui.dashboard;
-
-import androidx.lifecycle.ViewModel;
-
-public class DashboardViewModel extends ViewModel {
-    public DashboardViewModel() {
-    }
-}
