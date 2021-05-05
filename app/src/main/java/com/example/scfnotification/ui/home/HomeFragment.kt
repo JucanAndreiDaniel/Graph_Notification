52f29f25078064f859703728bb96820fdaa21b15
Gabriel Mareș <59911676+GabrielMares00@users.noreply.github.com>
2021-05-05 21:40:02 +0300

    HomeFragment replace ListView with RecyclerView WIP

--- app/src/main/java/com/example/scfnotification/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/scfnotification/ui/home/HomeFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.*
 import android.widget.*
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.scfnotification.R
 
 class HomeFragment : Fragment() {
 
-    private lateinit var adapter: ArrayAdapter<*>
+    private lateinit var recyclerView: RecyclerView;
+//    private lateinit var adapter: RecyclerView.Adapter<MyAdapter.MyViewHolder>
+    private lateinit var currencyStringArray: Array<String>;
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -16,42 +19,31 @@ class HomeFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
 
-//        val BitcoinArray = arrayOf("Bitcoin", "eth", "polka")
-
         val root = inflater.inflate(R.layout.fragment_home, container, false)
 
-        adapter = ArrayAdapter(this.requireContext(), android.R.layout.simple_list_item_1,
-                resources.getStringArray(R.array.countries_array))
+        currencyStringArray = resources.getStringArray(R.array.countries_array);
 
-        val listView = root.requireViewById<ListView>(R.id.lv_listView)
-        listView.adapter = adapter
-        listView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
-            Toast.makeText(activity?.applicationContext, parent?.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show()
-        }
-
-        val emptyTextView = root.requireViewById<TextView>(R.id.tv_emptyTextView)
-
-        listView.emptyView = emptyTextView
+        recyclerView = root.findViewById(R.id.rv_recyclerView);
 
         return root
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        val search = menu.findItem(R.id.nav_search)
-        val searchView = search?.actionView as SearchView
-
-        searchView.queryHint = "Search for something"
-
-        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                adapter.filter.filter(newText)
-                return true
-            }
-        })
-    }
+//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+//        super.onCreateOptionsMenu(menu, inflater)
+//        val search = menu.findItem(R.id.nav_search)
+//        val searchView = search?.actionView as SearchView
+//
+//        searchView.queryHint = "Search for something"
+//
+//        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+//            override fun onQueryTextSubmit(query: String?): Boolean {
+//                return false
+//            }
+//
+//            override fun onQueryTextChange(newText: String?): Boolean {
+//                adapter.filter.filter(newText)
+//                return true
+//            }
+//        })
+//    }
 }
